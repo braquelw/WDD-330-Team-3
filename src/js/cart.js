cf4ca77efd9dae5554e14077d4f35c484c0abe55
braquelw <appaloosarider7@gmail.com>
2023-05-04 11:36:36 -0600

    added more features to delete button

--- src/js/cart.js
+++ src/js/cart.js
@@ -22,10 +22,27 @@ function cartItemTemplate(item) {
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
   <p class="cart-card__quantity">qty: 1</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
-  <button class="deleteBtn"><span data-id=${item.Id}>x</span></button>
+  <button class="deleteBtn" data-id="${item.Id}"><span>x</span></button>
 </li>`;
 
   return newItem;
 }
 
 renderCartContents();
+
+
+// delete item from cart
+document.querySelector(".deleteBtn").addEventListener("click", function() {
+  let dataID = this.getAttribute('data-id');
+  const cartItems = getLocalStorage('so-cart');
+  console.log(cartItems);
+  for(let i=0; i < cartItems.length; i++) {
+    if (cartItems[i].Id == dataID) {
+      cartItems.splice(i, 1);
+      localStorage.removeItem(i);
+      localStorage.setItem('so-cart', JSON.stringify(cartItems));
+      }
+      renderCartContents();
+    return;
+  }
+});
