b6a5e165089950ef15cde9bd9f5b149d76776ee5
jorgechvz <cha22060@byui.edu>
2023-05-12 11:41:54 -0500

    Fixed Main

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-import { default as productList } from "./productList.mjs";
-
-productList('products-list', 'tents');
-=======
 import productList from './productList.mjs';
 
 productList('.product-list', 'tents');
->>>>>>> 4271eaa000454477a38ca8cce32c033cfb6146d6
